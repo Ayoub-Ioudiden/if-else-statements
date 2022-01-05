bd0e3917d68f2d5c39511aeae180093fd1f44df4
Ayoub-Ioudiden <93585615+Ayoub-Ioudiden@users.noreply.github.com>
2022-01-05 14:54:16 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const myAge = 18;
+
+if (myAge >= 18) {
+    document.write("you are allowed in");
+} else {
+    document.write("sorry, you are not allowed in");
+
+}
+
+const isFemale = false;
+
+if (isFemale == true) {
+    document.write("this is a female guest");
+} else {
+    document.write("this is NOT a female guest");
+}
+
+
+const driverStatus = designated;
+
+if (driverStatus == designated) {
+    console.log("you are allowed to drive");
+} else {
+    console.log("you are too drunk to drive");
+}
